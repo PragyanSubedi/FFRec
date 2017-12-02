ab62e5cbb9f8187ab32216fa12361711ca7b288c
Pragyan Subedi <pragyansubedi@gmail.com>
2017-12-02 16:14:44 +0545

    Added tkinter

--- face_recognizer.py
+++ face_recognizer.py
@@ -3,16 +3,15 @@ import cv2
 import sqlite3
 import urllib
 
+#Global variables
 fontFace = cv2.FONT_HERSHEY_SIMPLEX
 fontScale = 1
 fontColor = (255, 0, 0)
-# for record not found case
 fontColor1 = (0, 0, 255)
+
 
 url='http://192.168.1.100:8080/shot.jpg'
 detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-# img = cv2.imread('images/download.jpeg', cv2.IMREAD_COLOR)
-
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 recognizer.read('trainer/trainer.yml')
 cascadePath = "haarcascade_frontalface_default.xml"
@@ -29,34 +28,40 @@ def getProfile(Id):
     conn.close()
     return profile
 
-
-#font = cv2.InitFont(cv2.CV_FONT_HERSHEY_SIMPLEX, 1, 1, 0, 1, 1)
-while True:
+def recognizeFace():
+    # while True:
+    global fontFace
+    global fontColor
+    global fontColor
+    global fontColor1
+    fontColor1 = (0, 0, 255)
+    fontFace = cv2.FONT_HERSHEY_SIMPLEX
+    fontScale = 1
+    fontColor = (255, 0, 0)
     imgResp=urllib.urlopen(url)
+
     #change into bytearray of unsigned integer type
     imgNp=np.array(bytearray(imgResp.read()),dtype=np.uint8)
+
     #Decode numpy array to opencv2 image
     img=cv2.imdecode(imgNp,-1)
 
+    cv2image = cv2.cvtColor(img, cv2.COLOR_BGR2RGBA)
     gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     faces=faceCascade.detectMultiScale(gray, 1.2,5)
     for(x,y,w,h) in faces:
 
         Idu, conf = recognizer.predict(gray[y:y+h, x:x+w])
-        cv2.putText(img,str(conf), (x, y + h + 120 ), fontFace, fontScale, fontColor1)
+        cv2.putText(cv2image,str(conf), (x, y + h + 120 ), fontFace, fontScale, fontColor1)
         if (conf<80):
-            cv2.rectangle(img, (x, y), (x + w, y + h), (225, 0, 0), 2)
+            cv2.rectangle(cv2image, (x, y), (x + w, y + h), (225, 0, 0), 2)
             profile = getProfile(Idu)
             if(profile!=None):
-                cv2.putText(img, str(profile[1]), (x,y+h+30),fontFace, fontScale, fontColor)
-                cv2.putText(img, str(profile[2]), (x, y + h + 60), fontFace, fontScale, fontColor)
-                cv2.putText(img, str(profile[3]), (x, y + h + 90), fontFace, fontScale, fontColor)
+                cv2.putText(cv2image, str(profile[1]), (x,y+h+30),fontFace, fontScale, fontColor)
+                cv2.putText(cv2image, str(profile[2]), (x, y + h + 60), fontFace, fontScale, fontColor)
+                cv2.putText(cv2image, str(profile[3]), (x, y + h + 90), fontFace, fontScale, fontColor)
                 #cv2.putText(img, str(profile[4]), (x, y + h + 120), fontFace, fontScale, fontColor)
         else:
-            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 225), 2)
-            font = cv2.FONT_HERSHEY_SIMPLEX
+            cv2.rectangle(cv2image, (x, y), (x + w, y + h), (0, 0, 225), 2)
             # cv2.putText(img,"Not Found", (x, y + h + 30 ), fontFace, fontScale, fontColor1)
-    cv2.imshow('frame', img)
-    if ord('q') == cv2.waitKey(1):
-        break
-cv2.destroyAllWindows()
+    return cv2image
